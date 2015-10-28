6513c26ef24b1ac522233d5046abf6fb91d0d6c0
Vladimir Mladenovic <vladimir.mladenovic@etondigital.com>
2015-10-28 19:20:47 +0100

    security fix

--- Twig/EDBlogExtension.php
+++ Twig/EDBlogExtension.php
@@ -246,6 +246,7 @@ class EDBlogExtension extends \Twig_Extension
 
     public function displayLinks($value)
     {
+        $value = strip_tags($value);
         //match valid url
         $urlRegEx="/\(?(?:(http|https|ftp):\/\/)?(?:((?:[^\W\s]|\.|-|[:]{1})+)@{1})?((?:www.)?(?:[^\W\s]|\.|-)+[\.][^\W\s]{2,4}|localhost(?=\/)|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::(\d*))?([\/]?[^\s\?]*[\/]{1})*(?:\/?([^\s\n\?\[\]\{\}\#]*(?:(?=\.)){1}|[^\s\n\?\[\]\{\}\.\#]*)?([\.]{1}[^\s\?\#]*)?)?(?:\?{1}([^\s\n\#\[\]]*))?([\#][^\s\n]*)?\)?/";
         preg_match_all($urlRegEx, $value,$matches);

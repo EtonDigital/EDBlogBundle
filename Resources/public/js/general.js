f18d10f3fdab11e7682d64b52a37647392fdd418
Jayes <jayesbe@users.noreply.github.com>
2016-01-13 08:34:43 -0500

    Update general.js
    Add toolbar option to view source code

--- Resources/public/js/general.js
+++ Resources/public/js/general.js
@@ -6,10 +6,11 @@ $(document).ready(function(){
     plugins: [
         "advlist autolink link lists charmap print preview hr anchor pagebreak",
         "searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking",
-        "table contextmenu directionality emoticons paste textcolor"
+        "table contextmenu directionality emoticons paste textcolor",
+        "code"
     ],
     toolbar1: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect",
-    toolbar2: "| link unlink anchor | forecolor backcolor  | print preview code ",
+    toolbar2: "| link unlink anchor | forecolor backcolor  | print preview code | code",
     image_advtab: true,
     relative_urls: false,
     remove_script_host: false
